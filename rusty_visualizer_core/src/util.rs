37cfe8ba45d99ba1eda466f6ca0f61fd37bf79c5
Ricky12Awesome <rickygraveen@gmail.com>
2021-04-19 19:10:17 -0500

    fixed cstr

--- rusty_visualizer_core/src/util.rs
+++ rusty_visualizer_core/src/util.rs
@@ -4,5 +4,5 @@ pub type AnyErrorResult<T> = std::result::Result<T, AnyError>;
 #[allow(unused_macros)]
 #[macro_export]
 macro_rules! cstr {
-    ($($arg:tt)*) => {&*std::ffi::CString::new(format!("")).ok().map(|it| it.into_boxed_c_str()).unwrap()};
+    ($($arg:tt)*) => {&*std::ffi::CString::new(format!($($arg)*)).ok().map(|it| it.into_boxed_c_str()).unwrap()};
 }
